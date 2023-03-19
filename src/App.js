0dacbb160f164e7fe83d87606d97c54acdeafe87
EricChukoltyansky <kazohelmet@gmail.com>
2023-03-19 16:25:42 +0200

    deployment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const api = process.env === "production" ? "" : "http://localhost:5000";
+  // const api = process.env === "production" ? "" : "http://localhost:5000";
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    console.log("api", api);
-    fetch("https://tiny-purse-elk.cyclic.app/api")
+    // console.log("api", api);
+    fetch("https://chartreuse-green-starfish-cuff.cyclic.app/api")
       .then((res) => res.json())
       .then((data) => setUsers(data.users));
-  }, [api]);
+  }, []);
 
   return (
     <div className="App">
@@ -25,9 +25,3 @@ function App() {
 
 export default App;
 
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/EricChukoltyansky/reactDeployToNetlifyWithBackend.git
-// git push -u origin main
